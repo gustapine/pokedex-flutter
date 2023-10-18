e6235646801aa71caace2f57c53e965566663d07
gustapine <gustavohd34@gmail.com>
2023-10-18 19:13:01 -0300

    first commit

---
+++ lib/models/pokemon.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:pokedex/utils/constants.dart';
+
+class Pokemon {
+  String url;
+  String name;
+
+  Pokemon({required this.url, required this.name});
+
+  get id {
+    var urlParts = url.split('/');
+    var id = urlParts.reversed.skip(1).first;
+    return int.parse(id);
+  }
+
+  get image => '$pokemonImgUrl/$id.png';
+
+  factory Pokemon.fromJson(Map<String, dynamic> jsonData) {
+    return Pokemon(url: jsonData['url'], name: jsonData['name']);
+  }
+}
